ac03939cc7c0d349a8001dba6a4d429d58af594c
Toxic.Co <104733482+ItsToxicGGAlt@users.noreply.github.com>
2022-08-06 19:55:37 +0200

    using virions

--- src/Laith98Dev/LevelSystem/Main.php
+++ src/Laith98Dev/LevelSystem/Main.php
@@ -45,8 +45,8 @@ use pocketmine\utils\TextFormat as TF;
 
 use pocketmine\command\{Command, CommandSender};
 
-use Laith98Dev\LevelSystem\libs\jojoe77777\FormAPI\SimpleForm;
-use Laith98Dev\LevelSystem\libs\jojoe77777\FormAPI\CustomForm;
+use Vecnavium\FormsUI\SimpleForm;
+use Vecnavium\FormsUI\CustomForm;
 
 class Main extends PluginBase 
 {
